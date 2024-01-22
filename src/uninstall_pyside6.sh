2c26266a88bf954e469e6ee6f366d89620d9c55f
Fynn7 <fynnandreas7@gmail.com>
2024-01-22 21:29:53 +0100

    uninstall_pyside bash and instruction

--- src/uninstall_pyside6.sh
+++ src/uninstall_pyside6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of packages to uninstall
-packages=("PySide6" "PySide6-tools" "PySide6-examples")
+packages=("PySide6" "shiboken6" "PySide6-Essentials" "PySide6-Addons")
 
 # Uninstall each package
 for package in "${packages[@]}"
