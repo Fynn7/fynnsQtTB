930d7434b5803432a724821d3e6601b47ba035bc
Fynn7 <fynnandreas7@gmail.com>
2024-05-06 22:42:44 +0200

    add class FileConverter

--- src/components/tools/dsToolBox/ds.py
+++ src/components/tools/dsToolBox/ds.py
@@ -1,8 +1,53 @@
 import numpy as np
 import pandas as pd
+import traceback
 
-def clean(df:pd.DataFrame):
-    pass
+from ...templates.fynnsComponents import _select_file,_save_file
 
-def analyze(df:pd.DataFrame):
-    pass
+class DataHandler:
+    '''
+    Handle data cleaning, analysis, and visualization
+    '''
+    def __init__(self) -> None:
+        pass
+
+    def clean(cls,df:pd.DataFrame):
+        pass
+
+    def analyze(cls,df:pd.DataFrame):
+        pass
+
+class FileConverter:
+    '''
+    Convert data to different file formats
+    '''
+    def __init__(self,df:pd.DataFrame) -> None:
+        pass
+    
+    @staticmethod
+    def read_data()->pd.DataFrame|None:
+        file_path=_select_file()
+        # cut the file extension
+        if file_path:
+            return eval(f"pd.read_{file_path.split('.')[-1]}")(file_path)
+    
+    @classmethod
+    def converted(cls,target_type:str)->pd.DataFrame:
+        '''
+        Convert a file to a different file format
+        
+        args:
+        - target_type: "excel" | "json" | "csv" 
+        '''
+        data=cls.read_data()
+        if data is not None:
+            try:
+                data=eval(f"data.to_{target_type}")()
+                save_file_path=_save_file(placeholder=f"untitled.{target_type}")
+                if save_file_path:
+                    with open(save_file_path,'w') as f:
+                        f.write(data)
+            except Exception:
+                print(traceback.format_exc())
+                raise Exception("Invalid target file type. Should be among 'excel', 'json', or 'csv'")
+            return data
--- src/components/tools/dsToolBox/dsToolBox.py
+++ src/components/tools/dsToolBox/dsToolBox.py
@@ -4,19 +4,21 @@ from PySide6.QtWidgets import (
     QDialog,
     QTableWidgetItem,
     QMessageBox,
-    QScrollArea
+    QScrollArea,
+    QMenu,
+    QLabel
 )
 from PySide6.QtCore import (
     Qt,
+    Slot
 )
 from PySide6.QtGui import (
     QAction,
-
 )
 
 
 from baseWindow import BaseWindow
-import ds
+from . import ds
 
 class DSToolBox(BaseWindow):
     '''
@@ -33,7 +35,36 @@ class DSToolBox(BaseWindow):
     def setupUi(self):
         self.scroll_area=QScrollArea()
         self.setCentralWidget(self.scroll_area)
+        self.info_label=QLabel("Select a file to convert")
+        self.scroll_area.setWidget(self.info_label)
         self.scroll_area.setWidgetResizable(True)
 
     def setupMenubar(self):
+        self.addBasicMenus(False)
         menubar = self.getCurrentMenubar()
+        
+        # File Converter Menu
+        file_converter_menu = QMenu("File Converter",self)
+
+        toCSV_action = QAction("To CSV",self)
+        toCSV_action.triggered.connect(lambda: self.handle_file_converter("csv"))
+
+        toExcel_action = QAction("To Excel",self)
+        toExcel_action.triggered.connect(lambda: self.handle_file_converter("excel"))
+        toExcel_action.setEnabled(False)
+
+        toJSON_action = QAction("To JSON",self)
+        toJSON_action.triggered.connect(lambda: self.handle_file_converter("json"))
+
+        file_converter_menu.addAction(toCSV_action)
+        file_converter_menu.addAction(toExcel_action)
+        file_converter_menu.addAction(toJSON_action)
+
+        menubar.addMenu(file_converter_menu)
+    
+    @Slot()
+    def handle_file_converter(self,target_type:str):
+        try:
+            self.info_label.setText(ds.FileConverter.converted(target_type))
+        except Exception as e:
+            QMessageBox.critical(self,"Unknown Error",str(e))
